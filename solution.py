238fc663a7eb468e79d09a21a92086f97005ef40
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-09 00:47:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,6 +10,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.03
-    #statistic, pvalue = ks_2samp(x, y)
+    #statistic, pvalue = ks_2samp(x, y) #3/4
     pvalue = MMD(compute_kernel="poly").test(x, y)[1]
     return pvalue < alpha
