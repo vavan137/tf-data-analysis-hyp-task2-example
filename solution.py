1f97fc2d2aa7a851b55a8933c472775c134b8f5a
vavan137 <113305894+vavan137@users.noreply.github.com>
2023-04-09 00:40:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+#from scipy.stats import ks_2samp
+from hyppo.ksample import MMD
 
 chat_id = 5463739632 # Ваш chat ID, не меняйте название переменной
 
@@ -9,5 +10,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 0.03
-    statistic, pvalue = ks_2samp(x, y)
+    #statistic, pvalue = ks_2samp(x, y)
+    pvalue = MMD(compute_kernel="poly").test(x, y)[1]
     return pvalue < alpha
